b31011566c0b021087fa1f930edde239bd2b9e21
Ning Yu <ningyu321@gmail.com>
2020-09-07 00:22:11 +0800

    opt into future dep gap

---
+++ components/SingleFileUploader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Single File Uploader</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ stories/SingleFileUploader.stories.js
@@ -0,0 +1,3 @@
+export default { title: 'SingleFileUploader' }
+
+export const singleFileUploader = () => '<SingleFileUploader />'
